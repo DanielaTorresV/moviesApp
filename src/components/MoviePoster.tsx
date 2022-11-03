9730f26405ec76c00bb4b27fdbbe40743e15c7f6
Daniela Torres <dannytorres0211@gmail.com>
2022-11-03 16:11:36 -0500

    feat: create initial page to show dtails

--- src/components/MoviePoster.tsx
+++ src/components/MoviePoster.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Image, Text, View } from 'react-native';
+import { Image, TouchableOpacity, View } from 'react-native';
 import { Movie } from '../interfaces/movieInterface';
 import { styles } from '../theme/appTheme';
+import { useNavigation } from '@react-navigation/core';
+import { RootStackParams } from '../navigation/StackNavigator';
 
 interface Props {
   movie: Movie;
@@ -11,14 +13,23 @@ interface Props {
 
 const MoviePoster = ({ movie, height= 420, width= 300 } : Props) => {
 
-const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+const navigation = useNavigation<any>();
 
   return (
-    <View style={{
-      height,
-      width,
-      marginHorizontal: 8
-    }} >
+    <TouchableOpacity
+      activeOpacity={ 0.8 }
+      style={{
+        height,
+        width,
+        marginHorizontal: 2,
+        paddingBottom: 20,
+        paddingHorizontal: 7,
+
+      }}
+      onPress={() => navigation.navigate('DetailScreen', movie )}
+    >
       <View style={ styles.containerMoviePosterImage } >
         <Image 
           source={{ uri }}
@@ -26,7 +37,7 @@ const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
         />
       </View>
       
-    </View>
+    </TouchableOpacity>
   )
 }
 
--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { StackScreenProps } from '@react-navigation/stack';
+import { RootStackParams } from '../navigation/StackNavigator';
 
-const DetailScreen = () => {
+interface Props extends StackScreenProps<RootStackParams, 'DetailScreen'>{};
+
+const DetailScreen = ( { route }: Props ) => {
+
+  const movie = route.params;
+
   return (
     <View>
       <Text>Detalles</Text>
