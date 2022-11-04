a8cf43dcc337c1e0d4c5286bc2972cb4d2104ff0
Daniela Torres <dannytorres0211@gmail.com>
2022-11-04 12:02:24 -0500

    feat: read colors

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-import { ActivityIndicator, Text, View, Dimensions, ScrollView } from 'react-native';
+import { ActivityIndicator, View, Dimensions, ScrollView } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Carousel from 'react-native-snap-carousel';
-import GradientBG from '../components/GradientBG';
+import ImageColors from 'react-native-image-colors';
 
+import GradientBG from '../components/GradientBG';
 import HorizontalSlider from '../components/HorizontalSlider';
 import MoviePoster from '../components/MoviePoster';
 import useMovies from '../hooks/useMovies';
@@ -16,6 +17,14 @@ const HomeScreen = () => {
 
   const { nowPlaying, popular, topRated, upcoming, isLoading } = useMovies();
   const { top } = useSafeAreaInsets();
+
+  const getPosterColors = async( index: number ) => {
+    const movie = nowPlaying[index];
+    const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    
+    const colors = await ImageColors.getColors( uri, {});
+    console.log(colors);
+  }
 
   if ( isLoading ) {
     return(
@@ -38,6 +47,7 @@ const HomeScreen = () => {
               sliderWidth= { windowWidth }
               itemWidth= { 300 }
               inactiveSlideOpacity={ 0.9 }
+              onSnapToItem={ ( index ) => getPosterColors( index )}
             />
           </View>
 
